dcc67bfd5a350ecc34a73cef492d0eb560bb3081
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-01-26 20:36:13 +0900

    Initial commit

---
+++ src/main/java/com/cos/blog_prac/BlogPracApplication.java
@@ -0,0 +1,13 @@
+package com.cos.blog_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/blog_prac/BlogPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.blog_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
